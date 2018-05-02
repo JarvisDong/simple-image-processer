cffad42d26a51ed1f827b3c06172ab19c48a07d9
Jarvis Dong <jarvishdong@gmail.com>
2018-05-02 02:24:23 -0700

    3a finished

--- Project3/project3a/3A_c.c
+++ Project3/project3a/3A_c.c
@@ -2,33 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct 
-{
+typedef struct {
 	unsigned char r;
 	unsigned char g;
 	unsigned char b;
 } Pixel;
 
-typedef struct
-{
+typedef struct {
 	Pixel *pixel;
 	int width;
 	int height;
 
 } Image;
 
-Image *
-ReadImage(char *filename)
-{
+Image *ReadImage(char *filename) {
 	char magicNum[128];
 	int width, height, maxval;
 
-	Image *image = (Image *) malloc(sizeof(Image)); //need a free()
-	FILE *f_in = fopen(filename, "r");
+	FILE *f_in;
+	f_in = fopen(filename, "r");
  	fscanf(f_in, "%s\n%d %d\n%d\n", magicNum, &width, &height, &maxval); //magicNum = "P6", width = 1786, height = 1344, maxval = 255
+ 	Image *image = (Image *) malloc(sizeof(Image));
  	image->width = width;
  	image->height = height;
- 	image->pixel = (Pixel *) malloc(width*height*sizeof(Pixel)); //need a free()
+ 	image->pixel = (Pixel *) malloc(width*height*sizeof(Pixel));
+ 	fread(image->pixel, sizeof(Pixel), height*width, f_in);
  	fclose(f_in);
  	return image;
 }
@@ -36,34 +34,50 @@ ReadImage(char *filename)
 
 void WriteImage(Image *img, char *filename)
 {
-	FILE *f_out = fopen(filename, "w");
+	FILE *f_out;
+	f_out = fopen(filename, "w");
 	fprintf(f_out, "P6\n%d %d\n%d\n", img->width, img->height, 255);
+	fwrite(img->pixel, sizeof(Pixel), img->height*img->width, f_out);
 	fclose(f_out);
 }
 
-Image *
-YellowDiagonal(Image *input)
-{	
-	for (int i=0; i<input->width; i++) {
-		for (int j=0; j<input->height; j++) {
+Image *YellowDiagonal(Image *input) {	
+	int w, h;
+	w = input->width;
+	h = input->height;
+
+	//make a copy of original image
+	Image *copy = (Image *) malloc(sizeof(Image));
+	copy->width = w;
+	copy->height = h;
+	copy->pixel = (Pixel *) malloc(w*h*sizeof(Pixel));
+
+	for (int i=0; i<w; i++) {
+		for (int j=0; j<h; j++) {
 			if (i==j) {
-				input->pixel[j*input->width+i].r = 255;
-				input->pixel[j*input->width+i].g = 255;
-				input->pixel[j*input->width+i].b = 0;
+				(copy->pixel)[j*w+i].r = 255;
+				(copy->pixel)[j*w+i].g = 255;
+				(copy->pixel)[j*w+i].b = 0;
 			}
 		}
 	}
+
+	return copy;
 }
 
-void freePixelofImage() {
-
+void freePixelandImage(Image *img) {
+	free(img->pixel);
+	free(img);
 }
 
 int main(int argc, char *argv[])
 {
    /* Read an image, apply yellow diagonal to it, then write */
-	Image *input = ReadImage(argv[1]);
-	Image *output = YellowDiagonal(input);
+	Image *input, *output;
+	input = ReadImage(argv[1]);
+	output = YellowDiagonal(input);
 	WriteImage(output, argv[2]);
+	freePixelandImage(input);
+	freePixelandImage(output);
 	return 0;
 }
