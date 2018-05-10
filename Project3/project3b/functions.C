f650ab604d03974659352eb11cf878cf523416fc
jarvisdong <jarvishdong@gmail.com>
2018-05-09 17:50:47 -0700

    test

--- Project3/project3b/functions.C
+++ Project3/project3b/functions.C
@@ -22,63 +22,56 @@ void WriteImage(char *filename, Image &img) {
 };
 
 void HalfSize(Image &input, Image &output) {
-	int w = input.width / 2;
-	int h = input.height / 2;
-	output.ResetSize(w, h);
+	output.ResetSize(input.width / 2, input.height / 2);
 
-	for (int i = 0; i < w; i++) {
-		for (int j = 0; j < h; j++) {
-			output.pixel[j*w+i] = input.pixel[2*(j*input.width+i)];
+	for (int i = 0; i < output.width; i++) {
+		for (int j = 0; j < output.height; j++) {
+			output.pixel[j*output.width+i] = input.pixel[2*(j*input.width+i)];
 		}
 	}
 };
 
 void LeftRightCombine(Image &leftInput, Image &rightInput, Image &output) {
 	// assume leftInput.height = rightInput.height.
-	int w = leftInput.width + rightInput.width;
-	int h = leftInput.height;
-	output.ResetSize(w, h);
+	output.ResetSize(leftInput.width + rightInput.width, leftInput.height);
 
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w / 2; j++) {
-			output.pixel[i*w+j] = leftInput.pixel[i*leftInput.width+j];
+	for (int i = 0; i < output.height; i++) {
+		for (int j = 0; j < output.width / 2; j++) {
+			output.pixel[i*output.width+j] = leftInput.pixel[i*leftInput.width+j];
 		}
-		for (int k = w / 2; k < w; k++) {
-			output.pixel[i*w+k] = rightInput.pixel[i*rightInput.width+k];
+		for (int k = output.width / 2; k < output.width; k++) {
+			output.pixel[i*output.width+k] = rightInput.pixel[i*rightInput.width+k];
 		}
 	}
 };
 
 void TopBottomCombine(Image &topInput, Image &bottomInput, Image &output) {
 	//assume topInput.width = bottomInput.width.
-	int w = topInput.width;
-	int h = topInput.height + bottomInput.height;
-	output.ResetSize(w, h);
+	output.ResetSize(topInput.width, topInput.height + bottomInput.height);
 
-	for (int i = 0; i < w; i++) {
-		for (int j = 0; j < h / 2; j++) {
-			output.pixel[j*w+i] = topInput.pixel[j*topInput.width+i];
+	for (int i = 0; i < output.width; i++) {
+		for (int j = 0; j < output.height / 2; j++) {
+			output.pixel[j*output.width+i] = topInput.pixel[j*topInput.width+i];
 		}
 	}
-	for (int i = 0; i < w; i++){
-		for (int j = h / 2; j < h; j++) {
-			output.pixel[j*w+i] = bottomInput.pixel[(j-h/2)*bottomInput.width+i];
+	for (int i = 0; i < output.width; i++){
+		for (int j = output.height / 2; j < output.height; j++) {
+			output.pixel[j*output.width+i] = bottomInput.pixel[(j-output.height/2)*bottomInput.width+i];
 		}
 	}
 };
 
 void Blend(Image &input1, Image &input2, double factor, Image &output) {
 	//assume two input images have same dimension.
-	int w = input1.width;
-	int h = input1.height;
-	output.ResetSize(w, h);
+	output.ResetSize(input1.width, input1.height);
+	double factor2 = double(1) - factor;
 	// unsigned char *blend[w*h*3];
 
-	for (int i = 0; i < w; i++) {
-		for (int j = 0; j < h; j++) {
-			output.pixel[j*w+i].r = input1.pixel[j*w+i].r*factor + input2.pixel[j*w+i].r*(1-factor);
-			output.pixel[j*w+i].g = input1.pixel[j*w+i].g*factor + input2.pixel[j*w+i].g*(1-factor);
-			output.pixel[j*w+i].b = input1.pixel[j*w+i].b*factor + input2.pixel[j*w+i].b*(1-factor);
+	for (int i = 0; i < output.width; i++) {
+		for (int j = 0; j < output.height; j++) {
+			output.pixel[j*output.width+i].r = (input1.pixel[j*input1.width+i].r)*factor + (input2.pixel[j*input2.width+i].r)*factor2;
+			output.pixel[j*output.width+i].g = (input1.pixel[j*input1.width+i].g)*factor + (input2.pixel[j*input2.width+i].g)*factor2;
+			output.pixel[j*output.width+i].b = (input1.pixel[j*input1.width+i].b)*factor + (input2.pixel[j*input2.width+i].b)*factor2;
 		}
 	}
 };
